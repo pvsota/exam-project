9dc9e1d9551c3964ffd796c78f949ad4a5e7d084
Adam Cichý <adam@MacBook-Air-de-Adam.local>
2018-06-12 13:17:43 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,81 @@
-let queArray = [];
-window.addEventListener("load", loadJson());
+//global variables
+let myChart;
+let data;
+let queLength;
+let peopleServed;
+let myLineChart;
+let i = 0; // counter for interval we set 
+let j = 0; // counter for lenght of data array
+
+// Chart.js library, we learned to use it from youtube tutorials and their documentation
+let canvas = document.getElementById('myChart');
+data = {
+    labels: ["0"],
+    datasets: [
+        {
+            label: "People in queue",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 5,
+            pointHitRadius: 10,
+            data: [0],
+        },
+        {
+          label: "People getting served",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(192,75,75,0.4)",
+          borderColor: "rgba(192,75,75,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(192,75,75,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(192,75,75,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 10,
+          data: [0],
+      }
+    ]
+};
+let option = {
+	showLines: true
+};
+myLineChart = Chart.Line(canvas,{
+	data:data,
+  options:option
+});
+//adding data of people in que dynamically, function can be find in chart.js documentation
+function addData(){
+  myLineChart.data.datasets[0].data[j] = queLength;
+  myLineChart.data.labels[j] = i;
+  myLineChart.update();
+}
+//adding data of people getting served dynamically 
+function addSecondData(){
+  myLineChart.data.datasets[1].data[j] = peopleServed;
+  myLineChart.data.labels[j] = i;
+  myLineChart.update();
+}
+
 function loadJson(){
 //get data
 let data = FooBar.getData();
@@ -7,17 +83,59 @@ let data = FooBar.getData();
 const json = JSON.parse(data);
 
 console.log(json);
-console.log("Bar named " + json.bar.name + " closing at: " + json.bar.closingTime);
-console.log("People in the que: " + json.queue.length);
-console.log("People getting served: " + json.serving.length);
-queArray.push(json.queue.length);
-console.log(queArray)
-
+// assign length of que and length of people getting served to variable we are going to use in function to add data dynamically
+queLength = json.queue.length ; 
+peopleServed = json.serving.length;
+// increasing the counters for function add data , i is for the interval, which we set for each 10 seconds and j is for the lenght of data array
+i = i + 10; 
+j ++;
+// calling add data functions of the length of que and length of people getting served
+addData();
+addSecondData();
 
 json.bartenders.forEach(element => {
     console.log(element.name +" is "+ element.status);
     
 });
+
+function getOrders(){
+  //get data
+let data = FooBar.getData();
+//transfer data to JSON
+const json = JSON.parse(data);
+//select the template from HTML to get the info about beers
+let template = document.querySelector("#que").content;
+let parent = document.querySelector(".in-que");
+
+let serveTemplate = document.querySelector("#serve").content;
+let serveParent = document.querySelector(".in-serve");
+
+parent.innerHTML = '';
+json.queue.forEach(e =>{
+  let clone = template.cloneNode(true);
+  clone.querySelector('.id').textContent = "Order nr. " + (e.id + 1);
+  e.order.forEach(i =>{
+    clone.querySelector('.order').textContent += i + " ";
+  })
+
+  parent.appendChild(clone);
+})
+serveParent.innerHTML = '';
+json.serving.forEach(e=>{
+  let clone = serveTemplate.cloneNode(true);
+  clone.querySelector('.serve-id').textContent = "Order nr. " + (e.id + 1);
+  e.order.forEach(i =>{
+    clone.querySelector('.serve-order').textContent += i + " ";
+  })
+
+  serveParent.appendChild(clone);
+
+})
+
+
+}
+getOrders();
+
 }
 function getBeers(){
     //get data
@@ -47,102 +165,21 @@ json.beertypes.forEach(element => {
     parent.appendChild(clone);
 });
 }
+//navigation links
+document.querySelector('#beers-section').addEventListener("click",()=>{
+  document.querySelector('.beers').classList.toggle('hidden');
+})
+
+
+loadJson();
 getBeers();
-setInterval(loadJson,1000);
+setInterval(loadJson,10000);
 
 
-// Chart.js
-let myChart = document.getElementById('myChart').getContext('2d');
 
-    // Global Options
-    Chart.defaults.global.defaultFontFamily = 'Lato';
-    Chart.defaults.global.defaultFontSize = 18;
-    Chart.defaults.global.defaultFontColor = '#777';
 
-    let massPopChart = new Chart(myChart, {
-      type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-      data:{
-        labels:['0', '10', '20', '30', '40', '50'],
-        datasets:[{
-          label:'People in queue',
-          data:[
-            15,
-            13,
-            4,
-            7,
-            2,
-            12
-          ],
-          //backgroundColor:'green',
-          backgroundColor:[
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-            'rgba(153, 102, 255, 0.6)',
-            'rgba(255, 159, 64, 0.6)',
-            'rgba(255, 99, 132, 0.6)'
-          ],
-          borderWidth:1,
-          borderColor:'#777',
-          hoverBorderWidth:3,
-          hoverBorderColor:'#000'
-        },
-        {
-            label:'People served',
-            data:[
-              10,
-              8,
-              6,
-              9,
-              5,
-              10
-            ],
-            //backgroundColor:'green',
-            backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
-            ],
-            borderWidth:1,
-            borderColor:'#777',
-            hoverBorderWidth:3,
-            hoverBorderColor:'#000'
-          }
-    ]
-      },
-      options:{
-        title:{
-          display:true,
-          text:'People in queue',
-          fontSize:25
-        },
-        legend:{
-          display:true,
-          position:'right',
-          labels:{
-            fontColor:'#000'
-          }
-        },
-        layout:{
-          padding:{
-            left:50,
-            right:0,
-            bottom:0,
-            top:0
-          }
-        },
-        tooltips:{
-          enabled:true
-        }
-      }
-    }
-    
-);
+
+
 
 
 
